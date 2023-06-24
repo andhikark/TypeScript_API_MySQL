01dec7550570070fa4f2e11c5eba6ddee2e80552
Andhika_RK <andhikarestu873@gmail.com>
2023-06-24 10:58:30 +0800

    add .env file and add register

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import { Request,Response } from 'express';
+import express ,{ Request,Response } from 'express';
+import dotenv from 'dotenv';
 import mysql from 'mysql';
+dotenv.config();
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -9,10 +10,10 @@ app.use(express.json());
 app.get('/details/:id', (req : Request, res : Response) => {
 
     var pool = mysql.createPool({
-        host    : '127.0.0.1',
-        user   : 'root',
-        password : 'Password@123',
-        database : 'sakila',
+        host    : process.env.HOST,
+        user   : process.env.USER,
+        password : process.env.PASSWORD,
+        database : process.env.DATABASE,
         connectionLimit : 10,
         multipleStatements : true
     })
@@ -53,10 +54,65 @@ app.get('/details/:id', (req : Request, res : Response) => {
         });
     });
 
-    res.send({
-        message: "Hello World!",
-        id: req.params.id,
-        name: req.params.name
+    // res.send({
+    //     message: "Hello World!",
+    //     id: req.params.id,
+    //     name: req.params.name
+    // });
+})
+
+app.post('/register', (req : Request, res : Response) => {
+
+    var pool = mysql.createPool({
+        host    : process.env.HOST,
+        user   : process.env.USER,
+        password : process.env.PASSWORD,
+        database : process.env.DATABASE,
+        connectionLimit : 10,
+        multipleStatements : true
+    })
+
+    pool.getConnection(function(err: any, conn:any) {
+        if(err){
+            console.log('entered into error');
+            res.send({
+                success: false,
+                statusCode : 500,
+                message : 'Getting error during connection'
+            })
+
+            return;
+        }
+
+        console.log('line 91');
+        console.log(req.body);
+
+        let sqlQuery =  `call registeruser(?,?,?)`;
+
+
+        //if you got connection 
+        conn.query(sqlQuery,[req.body.email,req.body.phone,req.body.password], function(err: any, rows: any) {
+            if(err){
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode : 400
+                });
+            }
+
+            console.log('line 103');
+             console.log(req.body);
+
+            //for simplicity just send the rows
+            res.send({
+                message : 'success',
+                statusCode: 200,
+                // data : rows
+            });
+
+            //close connection 
+            conn.release();
+        });
     });
 })
 
@@ -70,6 +126,6 @@ app.post('/Id/:id/Name/:name', (req : Request, res : Response) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
+app.listen(process.env.PORT, () => {
+    console.log(`The application is listening on port ${process.env.PORT}`);
 })
