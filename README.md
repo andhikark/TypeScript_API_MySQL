90802ecd2e60cd816dd1ac0a34ad697f19f6fb45
Andhika_RK <andhikarestu873@gmail.com>
2023-06-24 10:10:54 +0800

    create environment

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TypeScript_API_MySQL
+
+Create API using typescript and express, with MySQL as the database 
+
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { Request,Response } from 'express';
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req : Request, res : Response) => {
+    res.send('Hello World!');
+})
+
+app.post('/', (req : Request, res : Response) => {
+    res.send({
+        data: req.body
+    })
+});
+
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
+})
