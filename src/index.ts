3f85ef4f9a51d92dac0e36440339305167c7a01e
Andhika_RK <andhikarestu873@gmail.com>
2023-06-24 10:35:32 +0800

    create connection to database

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,72 @@
 import express from 'express';
 import { Request,Response } from 'express';
+import mysql from 'mysql';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req : Request, res : Response) => {
-    res.send('Hello World!');
+app.get('/details/:id', (req : Request, res : Response) => {
+
+    var pool = mysql.createPool({
+        host    : '127.0.0.1',
+        user   : 'root',
+        password : 'Password@123',
+        database : 'sakila',
+        connectionLimit : 10,
+        multipleStatements : true
+    })
+
+    pool.getConnection(function(err: any, conn:any) {
+        if(err){
+            console.log('entered into error');
+            res.send({
+                success: false,
+                statusCode : 500,
+                message : 'Getting error during connection'
+            })
+
+            return;
+        }
+
+        console.log('The id : ' + req.params.id);
+
+        //if you got connection 
+        conn.query('SELECT * FROM actor WHERE actor_id = ?',[req.params.id], function(err: any, rows: any) {
+            if(err){
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode : 400
+                });
+            }
+
+            //for simplicity just send the rows
+            res.send({
+                message : 'success',
+                statusCode: 200,
+                data : rows
+            });
+
+            //close connection 
+            conn.release();
+        });
+    });
+
+    res.send({
+        message: "Hello World!",
+        id: req.params.id,
+        name: req.params.name
+    });
 })
 
-app.post('/', (req : Request, res : Response) => {
+app.post('/Id/:id/Name/:name', (req : Request, res : Response) => {
     res.send({
-        data: req.body
+        data: req.body,
+        params: {
+            id: req.params.id,
+            name: req.params.name
+        }
     })
 });
 
